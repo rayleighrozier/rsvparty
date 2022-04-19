ddbbce6581bac90fd1a39b9d19254ed847805b6c
Santos <sg21894@gmail.com>
2022-04-19 04:54:45 -1000

    Added Home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import Home from "./components/Home";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
+
 import Dashboard from "./components/Dashboard";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
