878278a4f43fac9fa0231b876ddeb0c7f9bffd19
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-04-19 11:39:30 -0400

    updating rootReducer with combineReducers

--- src/reducers/guestReducer.js
+++ src/reducers/guestReducer.js
@@ -1,5 +1,5 @@
 import { TEST_GUEST } from "../action-types/index";
-const initialState = { test: "test" };
+const initialState = {};
 
 function guest(state = initialState, action) {
   switch (action.type) {
