96acf814859b19f1323068ff6cba90ab353d3a5e
Santos <sg21894@gmail.com>
2022-04-20 05:58:34 -1000

    Created Add Party form

--- src/actions/input.js
+++ src/actions/input.js
@@ -17,4 +17,18 @@ const captureSignIn = (e) => {
   };
   return input;
 };
-export { captureSignUp, captureSignIn };
+const captureAddParty = (e) => {
+  e.preventDefault();
+  let input = {
+    name: e.target.form[0].value,
+    date: e.target.form[1].value,
+    time: e.target.form[2].value,
+    address: e.target.form[3].value,
+    city: e.target.form[4].value,
+    state: e.target.form[5].value,
+    zip: e.target.form[6].value,
+    details: e.target.form[7].value,
+  };
+  return input;
+};
+export { captureSignUp, captureSignIn, captureAddParty };
