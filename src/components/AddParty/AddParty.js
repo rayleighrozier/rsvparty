babb97fc7d78d9d51a0178ff3bef7785b6c10031
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-04-20 10:57:42 -0400

    replaced SignIn state variable with token

--- src/components/AddParty/AddParty.js
+++ src/components/AddParty/AddParty.js
@@ -2,13 +2,22 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import PartyDetails from "./PartyDetails";
 import InviteGuests from "./InviteGuests";
+import Error from "../Error/Error";
+import { checkToken } from "../../actions/token";
 
 export default function AddParty() {
   const page = useSelector((state) => state.page);
+  const token = checkToken();
   return (
-    <div>
-      <h1>Add Party Form</h1>
-      {page === "partyDetails" ? <PartyDetails /> : <InviteGuests />}
-    </div>
+    <>
+      {token ? (
+        <div>
+          <h1>Add Party Form</h1>
+          {page === "partyDetails" ? <PartyDetails /> : <InviteGuests />}
+        </div>
+      ) : (
+        <Error />
+      )}
+    </>
   );
 }
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -3,19 +3,18 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { captureSignIn } from "../../actions/input";
 import { userSignIn } from "../../actions/supabase";
-import { SET_GUESTID, SET_PAGE, SET_SIGNEDIN } from "../../action-types/index";
+import { SET_GUESTID, SET_PAGE } from "../../action-types/index";
+import { checkToken } from "../../actions/token";
 
 export default function SignIn() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const sendSignIn = async (e) => {
     let input = captureSignIn(e);
-    let currentGuest = await guestSignIn(input.email, input.password);
-    let token = localStorage.getItem("supabase.auth.token");
-    console.log("token", token);
-    if (currentGuest.message !== "Invalid login credentials") {
+    let currentGuest = await userSignIn(input.email, input.password);
+    let token = checkToken();
+    if (token) {
       dispatch({ type: SET_GUESTID, payload: currentGuest.id });
-      dispatch({ type: SET_SIGNEDIN, payload: true });
       dispatch({ type: SET_PAGE, payload: "dashboard" });
       navigate("/dashboard");
     } else {
