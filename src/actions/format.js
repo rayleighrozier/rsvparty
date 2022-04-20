a8ad19779ac482b8d279e737330c451d07f8bdb2
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-04-20 16:39:29 -0400

    cleanup format.js

--- src/actions/format.js
+++ src/actions/format.js
@@ -12,23 +12,24 @@ const monthNames = [
   "November",
   "December",
 ];
-//change MM to actual month name (used in formateDate2)
+//change MM to actual month name (used in formateDate)
 const getMonthName = (data) => {
   let date = new Date(data);
   return monthNames[date.getMonth()];
 };
-//makes the date Month Day (ex.January 1)
+//makes the date Month Day (ex. January 1)
 const formatDate = (data) => {
   const monthName = getMonthName(data);
   const [year, month, day] = data.split("-");
   return [monthName, day].join(" ");
 };
-//makes the date MM/DD/YYYY
+//makes the date MM/DD/YYYY (ex. 01/01/2022)
 const formatDate2 = (data) => {
   const [year, month, day] = data.split("-");
   return [month, day, year].join("/");
 };
 
+//from HH:MM:SS to HH:MM (ex. 6:00 PM)
 const formatTime = (data) => {
   data = data.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [
     data,
