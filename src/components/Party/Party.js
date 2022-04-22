e7f15321d1f602dd446bb29ddf6815be6f3399ae
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-04-22 19:37:12 -0400

    adds token validation to party

--- src/components/Party/Party.js
+++ src/components/Party/Party.js
@@ -8,12 +8,14 @@ import { guestGetInfo, partyFindById } from "../../actions/supabase";
 import { formatDate, formatTime } from "../../actions/format";
 import PlaylistButton from "./PlaylistButton";
 import EditDetailsButton from "./EditDetailsButton";
+import { checkToken } from "../../actions/token";
 
 // Countdown, Details, Playlist, Supplies, Comments
 
 export default function Party() {
   const dispatch = useDispatch();
   const { partyId } = useParams();
+  const token = checkToken();
   const party = useSelector((state) => state.party);
   const guest = useSelector((state) => state.guest);
   const [host, setHost] = useState(false);
@@ -24,7 +26,6 @@ export default function Party() {
     data.time = formatTime(data.time);
     dispatch({ type: SET_PARTY, payload: data });
   };
-
   const checkHost = () => {
     if (guest.guestId === party.hostId) {
       setHost(true);
@@ -40,23 +41,27 @@ export default function Party() {
   let timeLeft = Date.now() - Date.parse(endDate);
   return (
     <div>
-      <h1>Party</h1>
-      <Countdown date={Date.now() + Math.abs(timeLeft)}>
-        <Completionist />
-      </Countdown>
-      {party ? (
-        <div>
-          <p>{party.name}</p>
-          <p>{party.date}</p>
-          <p>{party.time}</p>
-          <p>{party.details}</p>
-          <p>{party.location.address}</p>
-          <p>{party.location.city}</p>
-          <p>{party.location.state}</p>
-          <p>{party.location.zip}</p>
-          {host ? <EditDetailsButton /> : null}
-          {/* <PlaylistButton /> */}
-        </div>
+      {token ? (
+        <>
+          <h1>Party</h1>
+          <Countdown date={Date.now() + Math.abs(timeLeft)}>
+            <Completionist />
+          </Countdown>
+          {party ? (
+            <div>
+              <p>{party.name}</p>
+              <p>{party.date}</p>
+              <p>{party.time}</p>
+              <p>{party.details}</p>
+              <p>{party.location.address}</p>
+              <p>{party.location.city}</p>
+              <p>{party.location.state}</p>
+              <p>{party.location.zip}</p>
+              {host ? <EditDetailsButton /> : null}
+              {/* <PlaylistButton /> */}
+            </div>
+          ) : null}
+        </>
       ) : null}
     </div>
   );
