be6b9b6ea39c9394e45eb394989850582fdcf734
JAReichert <87733614+JAReichert@users.noreply.github.com>
2022-04-24 13:52:40 -0500

    Created error component and css for error page

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "./Error.css";
 
 export default function Error() {
   return (
-    <div>
-      <h1>404 Page</h1>
-    </div>
+    <>
+      <div className="error">
+        <img
+          className="chicken"
+          src="https://memegenerator.net/img/instances/61775477/party-foul.jpg"
+        ></img>
+        <br></br>
+      </div>
+      <div className="message">
+        <h2>Stop monkeying around and get back to the party!</h2>
+      </div>
+      <br></br>
+      <a className="linkBackToParty" href="">
+        Back to the party!
+      </a>
+    </>
   );
 }
