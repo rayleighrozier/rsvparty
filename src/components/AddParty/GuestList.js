c35b7a8fe39814903bcb3a692c6007baa77b8716
JAReichert <87733614+JAReichert@users.noreply.github.com>
2022-04-26 22:54:57 -0500

    styled AddParty components

--- src/components/AddParty/GuestList.js
+++ src/components/AddParty/GuestList.js
@@ -1,41 +1,44 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import "./InviteGuests.css";
 
 export default function GuestList() {
   const newParty = useSelector((state) => state.newParty);
   const host = useSelector((state) => state.guest);
   return (
-    <div className="guestList">
-      <p>Guest List</p>
-      {newParty.guestList
-        ? newParty.guestList.map((guest, index) => {
-            return (
-              <form className="guestForm">
-                <input name="name" value={guest.firstName} readOnly />
-                <input name="lastName" value={guest.lastName} readOnly />
-                <input name="email" value={guest.email} readOnly />
-                <input
-                  className="hide"
-                  name="partyId"
-                  value={newParty.details.partyId}
-                  readOnly
-                />
-                <input
-                  className="hide"
-                  name="partyName"
-                  value={newParty.details.name}
-                  readOnly
-                />
-                <input
-                  className="hide"
-                  name="hostName"
-                  value={host.firstName + " " + host.lastName}
-                  readOnly
-                />
-              </form>
-            );
-          })
-        : null}
-    </div>
+    <>
+      <div className="guestList">
+        <p className="guestListTitle">Guest List</p>
+        {newParty.guestList
+          ? newParty.guestList.map((guest, index) => {
+              return (
+                <form className="guestForm">
+                  <input name="name" value={guest.firstName} readOnly />
+                  <input name="lastName" value={guest.lastName} readOnly />
+                  <input name="email" value={guest.email} readOnly />
+                  <input
+                    className="hide"
+                    name="partyId"
+                    value={newParty.details.partyId}
+                    readOnly
+                  />
+                  <input
+                    className="hide"
+                    name="partyName"
+                    value={newParty.details.name}
+                    readOnly
+                  />
+                  <input
+                    className="hide"
+                    name="hostName"
+                    value={host.firstName + " " + host.lastName}
+                    readOnly
+                  />
+                </form>
+              );
+            })
+          : null}
+      </div>
+    </>
   );
 }
