dd0f428b157ca367719261cd3c9997379d22a60c
ciaracloud <ciara_cloud@yahoo.com>
2022-05-02 15:50:59 -0400

    adds minor styling updates to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
     <div className="homePage">
       <div className="homeImgContainer">
         <img className="RSVPimage" src={RSVPimage} />
-        <p className="homeText">TIME TO PARTY</p>
+        <p className="homeText homeTextPhrase">UNLEASH YOUR PARTY ANIMAL!</p>
         <p className="homeText homeText2">
           Built by Rayleigh Rozier, Santos Gutierrez, Jason Reichert, and Ciara
           Cloud
