82071c36751d54cb164d43fbf16d5356bc1d2380
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-05-01 21:42:15 -0400

    loading spinner!

--- src/components/Party/Party.js
+++ src/components/Party/Party.js
@@ -21,6 +21,7 @@ import Supplies from "../Supplies/Supplies";
 import HostButtons from "./HostButtons";
 import AdditionalDetails from "./AdditionalDetails";
 import CountdownTitles from "./CountdownTitles";
+import Loading from "../Loading/Loading";
 import "./Party.css";
 
 export default function Party() {
@@ -30,10 +31,10 @@ export default function Party() {
   const token = checkToken();
   const party = useSelector((state) => state.party);
   const guest = useSelector((state) => state.guest);
-
   const [host, setHost] = useState(false);
   const [attending, setAttending] = useState(null);
   const [invited, setInvited] = useState(false);
+  const [loading, setLoading] = useState(true);
   const endDates = party?.date + ` 2022 ` + party?.time;
   let timeLeft = Date.now() - Date.parse(endDates);
   let tl = 0;
@@ -104,57 +105,74 @@ export default function Party() {
     setPartyUnformatted();
     dispatch({ type: SET_PAGE, payload: "party" });
   }, []);
+
   useEffect(() => {
     checkHost();
     checkInvited();
     checkAttending();
-  }, [party]);
+    if (party && invited) {
+      setLoading(false);
+    } else if (party) {
+      setTimeout(() => setLoading(false), 3000);
+    }
+  }, [party, invited]);
 
   return (
     <>
-      {token ? (
-        invited ? (
-          <div className="party-main-container">
-            {" "}
-            <p className="timeToParty">TIME TO PARTY!</p>
-            <div className="party-top-container">
-              <div className="party-countdown-container">
-                <div className="party-countdown-numbers">
-                  <Countdown date={Date.now() + tl}>
-                    <Completionist />
-                  </Countdown>
+      {loading === false ? (
+        <>
+          {token ? (
+            invited ? (
+              <div className="party-main-container">
+                {" "}
+                <p className="timeToParty">TIME TO PARTY!</p>
+                <div className="party-top-container">
+                  <div className="party-countdown-container">
+                    <div className="party-countdown-numbers">
+                      <Countdown date={Date.now() + tl}>
+                        <Completionist />
+                      </Countdown>
+                    </div>
+                    <CountdownTitles />
+                  </div>
+                  {party ? (
+                    <PartyDetails host={host} setHost={setHost} />
+                  ) : null}
                 </div>
-                <CountdownTitles />
+                {host ? (
+                  <HostButtons />
+                ) : (
+                  <RSVPButtons
+                    attending={attending}
+                    setAttending={setAttending}
+                  />
+                )}
+                <div className="party-middle-container">
+                  <div className="party-middle-left">
+                    {party ? <AdditionalDetails /> : null}
+                    {party ? <Guests host={host} /> : null}
+                  </div>
+                  <div className="party-middle-right">
+                    {party ? <Comments /> : null}
+                  </div>
+                </div>
+                {party ? <Supplies host={host} /> : null}
               </div>
-              {party ? <PartyDetails host={host} setHost={setHost} /> : null}
-            </div>
-            {host ? (
-              <HostButtons />
             ) : (
-              <RSVPButtons attending={attending} setAttending={setAttending} />
-            )}
-            <div className="party-middle-container">
-              <div className="party-middle-left">
-                {party ? <AdditionalDetails /> : null}
-                {party ? <Guests host={host} /> : null}
+              <div className="party-not-on-list-container">
+                <div className="party-not-on-list">
+                  <p>Uh oh! Looks like you are not on the guest list.</p>
+                  <div className="party-not-on-list-button-container">
+                    <button onClick={backtoDashboard}>Go Back</button>
+                  </div>
+                </div>
               </div>
-              <div className="party-middle-right">
-                {party ? <Comments /> : null}
-              </div>
-            </div>
-            {party ? <Supplies host={host} /> : null}
-          </div>
-        ) : (
-          <div className="party-not-on-list-container">
-            <div className="party-not-on-list">
-              <p>Uh oh! Looks like you are not on the guest list.</p>
-              <div className="party-not-on-list-button-container">
-                <button onClick={backtoDashboard}>Go Back</button>
-              </div>
-            </div>
-          </div>
-        )
-      ) : null}
+            )
+          ) : null}
+        </>
+      ) : (
+        <Loading />
+      )}
     </>
   );
 }
